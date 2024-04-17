abc706a7e269196ccfd6c580242bd37ff9a7d649
SomaTakata <somatakata@gmail.com>
2024-04-18 08:27:17 +0900

    Barの固定

--- app/(dashboard)/dashboard/_components/NavBar.tsx
+++ app/(dashboard)/dashboard/_components/NavBar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="absolute -z-10 top-0  w-full h-14 bg-muted/55 text-primary-foreground py-2 px-6 flex items-center justify-end">
+    <nav className="fixed -z-10 top-0  w-full h-14 bg-white text-primary-foreground py-2 px-6 flex items-center justify-end">
       <UserButton />
     </nav>
   );
