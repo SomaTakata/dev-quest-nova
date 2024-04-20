32271f458d8af011d7274283bae7cb6dd7c2f478
SomaTakata <somatakata@gmail.com>
2024-04-20 21:11:23 +0900

    カード内のpaddingの調整

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
           {/* カードリスト */}
           <div className="gap-5 grid sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5  ">
             {/* カード */}
-            <div className=" h-52 w-full px-4 pb-4 pt-3 rounded-lg border border-card bg-card  shadow-sm ">
+            <div className=" h-52 w-full p-4 rounded-lg border border-card bg-card  shadow-sm ">
               <div className="flex justify-end mb-1">
                 <MoreHorizontal
                   size={22}
@@ -42,6 +42,68 @@ export default function Home() {
               </div>
               <div className="px-3">
                 <p className=" font-bold text-xl mb-7">株式会社サポーターズ</p>
+                <div className="flex  mb-2 gap-4 font-semibold items-center">
+                  <p className=" text-sm text-accent-foreground  ">期限</p>
+                  <p className="text-muted-foreground/50 text-sm ">
+                    2024年11月11日
+                  </p>
+                </div>
+                <div className="flex mb-8 gap-3 items-center">
+                  <p className="text-accent-foreground text-sm font-semibold">
+                    進捗
+                  </p>
+
+                  <Progress value={33} className="h-3 w-[80%]" />
+                </div>
+                <div className="text-right text-xs space-x-2 font-semibold ">
+                  <span className="text-accent-foreground ">作成日</span>
+                  <span className="text-muted-foreground/50 ">
+                    2024年11月11日
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className=" h-52 w-full p-4 rounded-lg border border-card bg-card  shadow-sm ">
+              <div className="flex justify-end mb-1">
+                <MoreHorizontal
+                  size={22}
+                  className="text-muted-foreground/50 "
+                  onClick={() => ""}
+                />
+              </div>
+              <div className="px-3">
+                <p className=" font-bold text-xl mb-8">株式会社サポーターズ</p>
+                <div className="flex  mb-2 gap-4 font-semibold items-center">
+                  <p className=" text-sm text-accent-foreground  ">期限</p>
+                  <p className="text-muted-foreground/50 text-sm ">
+                    2024年11月11日
+                  </p>
+                </div>
+                <div className="flex mb-8 gap-3 items-center">
+                  <p className="text-accent-foreground text-sm font-semibold">
+                    進捗
+                  </p>
+
+                  <Progress value={33} className="h-3 w-[80%]" />
+                </div>
+                <div className="text-right text-xs space-x-2 font-semibold">
+                  <span className="text-accent-foreground ">作成日</span>
+                  <span className="text-muted-foreground/50 ">
+                    2024年11月11日
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className=" h-52 w-full p-4 rounded-lg border border-card bg-card  shadow-sm ">
+              <div className="flex justify-end mb-1">
+                <MoreHorizontal
+                  size={22}
+                  className="text-muted-foreground/50 "
+                  onClick={() => ""}
+                />
+              </div>
+              <div className="px-3">
+                <p className=" font-bold text-xl mb-8">株式会社サポーターズ</p>
                 <div className="flex  mb-2 gap-4 font-semibold items-center">
                   <p className=" text-sm text-accent-foreground  ">期限</p>
                   <p className="text-muted-foreground/50 text-sm ">
