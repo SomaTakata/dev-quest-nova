9bce15080019d8e57450e5ea40e0f3625f8ec2e2
SomaTakata <somatakata@gmail.com>
2024-04-21 17:08:10 +0900

    import input

--- app/(dashboard)/dashboard/_components/DashBoardTitle.tsx
+++ app/(dashboard)/dashboard/_components/DashBoardTitle.tsx
@@ -6,6 +6,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { PlusCircle } from "lucide-react";
 import React from "react";
