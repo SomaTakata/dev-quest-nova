e96b8b244c9909041f69580d30dda0bd8bb77522
SomaTakata <somatakata@gmail.com>
2024-04-21 17:07:25 +0900

    add dialog

--- app/(dashboard)/dashboard/_components/DashBoardTitle.tsx
+++ app/(dashboard)/dashboard/_components/DashBoardTitle.tsx
@@ -1,3 +1,11 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { PlusCircle } from "lucide-react";
 import React from "react";
 
@@ -10,7 +18,44 @@ const DashBoardHeader = () => {
           今日は2024年5月10日です
         </p>
       </div>
-      <PlusCircle size={30} />
+      <Dialog>
+        <DialogTrigger>
+          <PlusCircle size={30} />
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input
+                id="name"
+                defaultValue="Pedro Duarte"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <Input
+                id="username"
+                defaultValue="@peduarte"
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
