7a9824ee72aad7ac9dec2e2f9d0a2ddb9547fc74
SomaTakata <somatakata@gmail.com>
2024-04-21 19:06:09 +0900

    CardListにCreateCard  の追加

--- app/(dashboard)/dashboard/_components/CardList.tsx
+++ app/(dashboard)/dashboard/_components/CardList.tsx
@@ -1,10 +1,19 @@
+import { Card } from "@/components/ui/card";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { PlusCircle } from "lucide-react";
 import React, { ReactNode } from "react";
-type Props = {
+import { CreateProjectForm } from "./CreateProjectForm";
+import CardForm from "./CardForm";
+import CreateCard from "./CreateCard";
+interface Props {
   children: ReactNode;
-};
-export const CardList = ({ children }: { children: React.ReactNode }) => {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+export const CardList = ({ children, open, setOpen }: Props) => {
   return (
-    <div className="gap-5 grid sm:grid-cols-1  md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5  ">
+    <div className="gap-5 grid sm:grid-cols-1  md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-4  ">
+      <CreateCard open={open} setOpen={setOpen} />
       {children}
     </div>
   );
