d751863b5f1a7ca663785632fa97e5401448bdb8
SomaTakata <somatakata@gmail.com>
2024-04-21 20:51:09 +0900

    propsの受け渡しと残りの日数の実装

--- app/(dashboard)/dashboard/_components/Card.tsx
+++ app/(dashboard)/dashboard/_components/Card.tsx
@@ -1,18 +1,34 @@
 import { Badge } from "@/components/ui/badge";
 import { Progress } from "@/components/ui/progress";
 import React from "react";
+interface ProjectItem {
+  companyName: string;
+  deadline: string;
+  url: string;
+}
+const Card = ({ companyName, deadline, url }: ProjectItem) => {
+  const calculateDaysUntilDeadline = (deadline: string) => {
+    const today = new Date();
+    const deadlineDate = new Date(deadline);
+    const timeDiff = deadlineDate.getTime() - today.getTime();
+    const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    return daysDiff;
+  };
 
-const Card = () => {
+  // Calculate days remaining to display
+  const daysUntilDeadline = calculateDaysUntilDeadline(deadline);
   return (
-    <div className=" w-full p-6 rounded-lg border border-card-foreground/10 bg-card  shadow-md ">
+    <div className=" w-full p-7 rounded-lg border border-card-foreground/10 bg-card  shadow-md ">
       <div className="px-3">
-        <div className="flex items-center justify-between mb-8">
-          <p className=" font-bold text-xl ">株式会社サポーターズ</p>
+        <div className="flex items-center justify-between mb-7">
+          <p className=" font-bold text-xl ">{companyName}</p>
           <Badge className="h-6 bg-green-400">提出済み</Badge>
         </div>
         <div className="flex  mb-2 gap-5 font-semibold items-center">
           <p className=" text-sm text-primary  ">締め切り</p>
-          <p className="text-muted-foreground/50 text-sm ">あと10日</p>
+          <p className="text-muted-foreground/50 text-sm ">
+            あと{daysUntilDeadline}日
+          </p>
         </div>
 
         <div className="flex mb-4 gap-5 items-center">
