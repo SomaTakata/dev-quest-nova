b545640f467d15ba4810feeb59bc9967a505e700
SomaTakata <somatakata@gmail.com>
2024-04-21 16:50:13 +0900

    cardの大きさを変更

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,16 +1,11 @@
-"use client";
-import { useClerk } from "@clerk/nextjs";
 import SideBar from "./_components/SideBar";
 import NavBar from "./_components/NavBar";
-import { MoreHorizontal, PlusCircle } from "lucide-react";
-import { Card } from "@/components/ui/card";
-import { useState } from "react";
+import { PlusCircle } from "lucide-react";
+
 import { Progress } from "@/components/ui/progress";
 import { Badge } from "@/components/ui/badge";
 
 export default function Home() {
-  const { user } = useClerk();
-
   return (
     <div className="h-full w-full flex relative">
       <SideBar />
@@ -21,8 +16,8 @@ export default function Home() {
           <div className="w-full flex justify-between items-center">
             <div className="flex flex-col">
               <h1 className="text-3xl font-bold">DashBoard</h1>
-              <p className="text-muted-foreground/15 font-bold text-xs">
-                Welcome back {user?.fullName}
+              <p className="text-muted-foreground/30 font-bold text-xs">
+                今日は2024年5月10日です
               </p>
             </div>
             <PlusCircle size={30} />
@@ -33,7 +28,7 @@ export default function Home() {
             {/* カード */}
             <div className=" w-full p-6 rounded-lg border border-card-foreground/10 bg-card  shadow-md ">
               <div className="px-3">
-                <div className="flex items-center justify-between mb-9">
+                <div className="flex items-center justify-between mb-8">
                   <p className=" font-bold text-xl ">株式会社サポーターズ</p>
                   <Badge className="h-6 bg-green-400">提出済み</Badge>
                 </div>
@@ -42,7 +37,7 @@ export default function Home() {
                   <p className="text-muted-foreground/50 text-sm ">あと10日</p>
                 </div>
 
-                <div className="flex mb-5 gap-5 items-center">
+                <div className="flex mb-4 gap-5 items-center">
                   <p className="text-primary text-sm font-semibold">進捗</p>
 
                   <Progress value={33} className="h-3 w-[60%]" />
