a0afe8c565658e157e26462a7247b2059dd61039
SomaTakata <somatakata@gmail.com>
2024-04-25 01:39:25 +0900

    Headerの変更

--- app/(dashboard)/dashboard/[id]/_components/EditorBoardTitle.tsx
+++ app/(dashboard)/dashboard/[id]/_components/EditorBoardTitle.tsx
@@ -1,19 +1,20 @@
 "use client";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import React, { useContext, useState } from "react";
 
-const EditorBoardHeader = ({ companyName }: { companyName: string }) => {
+import { Progress } from "@/components/ui/progress";
+
+interface EditorProps {
+  companyName: string;
+  deadline: string;
+}
+
+const EditorBoardHeader = ({ companyName, deadline }: EditorProps) => {
   return (
     <div className="w-full flex justify-between items-center">
-      <div className="flex flex-col">
-        <h1 className="text-3xl font-bold">{companyName}</h1>
-        <div className="h-full"></div>
+      <div className="flex flex-col w-[50%]">
+        <h1 className="text-foreground text-3xl font-bold">{companyName}</h1>
+        <p className="text-muted-foreground/50 font-bold text-sm">
+          締め切りは{deadline}です
+        </p>
       </div>
     </div>
   );
