dce2e97cdf76d4a253dcbee58f4bec351f5a29e1
SomaTakata <somatakata@gmail.com>
2024-06-08 23:34:13 +0900

    Pageの作成

--- app/(dashboard)/dashboard/projects/[id]/page.tsx
+++ app/(dashboard)/dashboard/projects/[id]/page.tsx
@@ -3,152 +3,162 @@ import React, { useState } from "react";
 import SideBar from "../../../_components/SideBar";
 import NavBar from "../../../_components/NavBar";
 import EditorBoardHeader from "./_components/EditorBoardTitle";
-import { usePathname } from "next/navigation";
-import { LoaderCircle, Trash2 } from "lucide-react";
+import { useParams } from "next/navigation";
+import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { cn } from "@/lib/utils";
-import { Separator } from "@/components/ui/separator";
+import { useAuth } from "@clerk/nextjs";
+import useSWR, { mutate } from "swr";
+import AccordionArea from "./_components/AccordionArea";
+
 interface ProjectItem {
   id: string;
-  companyName: string;
+  company_name: string;
   deadline: string;
   url: string;
+  questions: QuestionItem[];
 }
-const page = () => {
-  const pathname = usePathname();
-  const [values, setValues] = useState<ProjectItem[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [questionAI, setQuestionAI] = useState(false);
-  const [questionOwn, setQuestionOwn] = useState(true);
 
-  const pathParts = pathname.split("/");
-  const projectId = pathParts[2];
+interface QuestionItem {
+  id: string;
+  content: string;
+  answer: string;
+  checked: boolean;
+  locked: boolean;
+  created_at: string;
+}
 
-  const filteredProjects = values.filter((item) => item.id === projectId);
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const Page = () => {
+  const [showNewQuestion, setShowNewQuestion] = useState(false); // 新しい質問の表示制御用
+  const [newQuestionContent, setNewQuestionContent] = useState(""); // 新しい質問の内容
+  const { userId, isLoaded } = useAuth();
+
+  const { id } = useParams<{ id: string }>();
+
+  const { data, error } = useSWR<{ project: ProjectItem }>(
+    `/api/projects/${id}/questions`,
+    fetcher,
+  );
+
+  if (error) return <div>Failed to load project data</div>;
+  if (!data) return <div>Loading...</div>;
+
+  const project = data.project;
+  const questions = project.questions || []; // デフォルトで空の配列を設定
+
+  const handleAddQuestion = async () => {
+    if (!isLoaded || !userId) {
+      console.error("User ID is not loaded or available");
+      return;
+    }
+
+    try {
+      const response = await fetch(`/api/projects/${id}/questions`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: newQuestionContent,
+          answer: "",
+          checked: false,
+          locked: false,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create question");
+      }
+
+      const newQuestion = await response.json();
+
+      // SWRキャッシュを更新
+      mutate(`/api/projects/${id}/questions`);
+      setShowNewQuestion(false); // 新しい質問の入力欄を隠す
+      setNewQuestionContent(""); // 新しい質問の内容をリセット
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  console.log(project);
 
   return (
     <div className="h-full w-full flex relative">
       <SideBar />
       <NavBar />
-      <div className="z-10 w-full  h-full ml-52 pt-10  ">
+      <div className="z-10 w-full h-full ml-52 pt-10">
         <div className="p-10 flex flex-col">
           {/* 上部タイトルとアイコン */}
-          <EditorBoardHeader
-            companyName={filteredProjects[0]?.companyName}
-            deadline={filteredProjects[0]?.deadline}
-          />
-          <div className="h-10 w-full " />
+          {project && (
+            <EditorBoardHeader
+              company_name={project.company_name}
+              deadline={project.deadline}
+            />
+          )}
+          <div className="h-8 w-full" />
           <div className="">
-            {/* <p className="text-foreground/70 text-lg font-bold mb-1 ml-2">
-              質問
-            </p> */}
-            <div className="w-full relative flex flex-col p-8 rounded-lg border border-card-foreground/10 bg-card  shadow-md">
-              <div className="">
-                <div className="w-full px-7 font-bold">
-                  問1）
-                  MIXIのインターンシップで挑戦してみたいことや目的、目標を教えてください(500文字以内)*
-                </div>
-                <Trash2
-                  className="text-foreground/30
-                w-5 h-5 absolute right-6 top-8 "
-                />
-              </div>
-              <div className="px-7">
-                <div className="h-8 w-full" />
-                {!questionAI && !questionOwn && (
-                  <div className=" flex justify-between">
-                    <Button
-                      size="sm"
-                      className={`bg-primary/70 gap-3 ${isLoading ? "w-full" : "w-1/2"}`}
-                      onClick={() => {
-                        setIsLoading(true);
-                        setTimeout(() => {
-                          setIsLoading(false);
-                          setQuestionAI((prev) => !prev);
-                        }, 5000);
-                      }}
-                    >
-                      {isLoading && (
-                        <LoaderCircle className="animate-spin h-5 w-5" />
-                      )}
-                      質問を深堀する
-                    </Button>
-
-                    {!isLoading && (
-                      <Button
-                        size="sm"
-                        className="w-[50%] bg-primary/70"
-                        onClick={() => setQuestionOwn((prev) => !prev)}
-                      >
-                        そのまま記入する
-                      </Button>
-                    )}
-                  </div>
-                )}
-                {questionAI && (
-                  <div>
-                    <p className="text-foreground/40 text-sm font-bold mb-2 tracking-wide">
-                      以下の質問に回答してください。
-                    </p>
-                    <Accordion type="multiple">
-                      <AccordionItem
-                        value="item-1"
-                        className={cn(
-                          "bg-primary text-primary-foreground transition-color duration-200 hover:opacity-100 py-1 font-bold px-7 rounded-lg mt-1",
-                        )}
-                      >
-                        <AccordionTrigger className="font-medium text-sm tracking-wide">
-                          具体的にどのスキルや知識を身に着けたいですか？
-                        </AccordionTrigger>
-                        <AccordionContent>
-                          <div className="mt-1">
-                            <Textarea
-                              className="p-2 text-muted-foreground text-sm font-semibold"
-                              placeholder="回答を記入してください"
-                              disabled
-                              value="ありがとう"
-                            />
-                          </div>
-                          <Separator className="mt-6 " />
-                          <p className="font-medium my-3 tracking-wide">
-                            {" "}
-                            具体的にどのスキルや知識を身に着けたいですか？
-                          </p>
-                          <div className="mt-2">
-                            <Textarea
-                              className="p-2 text-muted-foreground text-sm font-semibold"
-                              placeholder="回答を記入してください"
-                            />
-
-                            <div className="flex justify-end mt-3">
-                              <Button
-                                size="xs"
-                                className="w-36 font-bold text-xs text-secondary border border-primary-foreground"
-                              >
-                                質問を深掘る
-                              </Button>
-                            </div>
-                          </div>
-                        </AccordionContent>
-                      </AccordionItem>
-                    </Accordion>
-                  </div>
-                )}
-                {questionOwn && (
-                  <Textarea
-                    placeholder="入力をしてください。"
-                    className="border-foreground focus-visible:border-input"
+            <div className="w-full relative flex flex-col p-6 rounded-lg border border-card-foreground/10 bg-card shadow-md">
+              {questions.length > 0 ? (
+                questions.map((question, index) => (
+                  <AccordionArea
+                    key={question.id}
+                    indexNumber={index}
+                    id={question.id}
+                    locked={question.locked}
+                    answerContent={question.answer}
+                    questionContent={question.content}
                   />
-                )}
-              </div>
+                ))
+              ) : (
+                <AccordionArea
+                  indexNumber={0}
+                  id=""
+                  locked={false}
+                  answerContent=""
+                  questionContent="新しい質問を入力してください"
+                />
+              )}
+
+              {showNewQuestion && (
+                <div className="mt-4">
+                  <Textarea
+                    placeholder="新しい質問を入力してください。"
+                    className="border-foreground focus-visible:border-input"
+                    value={newQuestionContent}
+                    onChange={(e) => setNewQuestionContent(e.target.value)}
+                  />
+                  <Button
+                    className="mt-4 bg-primary text-white"
+                    onClick={handleAddQuestion}
+                  >
+                    質問を追加
+                  </Button>
+                </div>
+              )}
+              {/* <div className="flex justify-start">
+                <Button
+                  className="mt-6 bg-[#FFFFFF] text-primary hover:text-[#FFFFFF] border border-primary"
+                  onClick={() => setShowNewQuestion(true)} // ボタンをクリックで新しい質問の入力欄を表示
+                >
+                  <Plus className="mr-2 h-4 w-4" />
+                  質問を追加
+                </Button>
+              </div> */}
             </div>
+            {questions.length > 0 && (
+              <div className="flex justify-end">
+                <Button
+                  className="mt-6 bg-[#FFFFFF] text-primary hover:text-[#FFFFFF] border border-primary"
+                  onClick={() => setShowNewQuestion(true)} // ボタンをクリックで新しい質問の入力欄を表示
+                >
+                  <Plus className="mr-2 h-4 w-4" />
+                  質問を追加
+                </Button>
+              </div>
+            )}
           </div>
         </div>
       </div>
@@ -156,4 +166,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
